cea44ca6133c32873d1c5d3cb30d5d28a2b12a97
lijunlin <julianlee107@hotmail.com>
2017-04-18 18:42:00 +0800

    example 1.0

--- recall.py
+++ recall.py
@@ -6,29 +6,16 @@ import time
 import shutil
 
 msg_dict={}
-@itchat.msg_register([NOTE])
-def SaveMsg(msg):
-    if not os.path.exists('./Revocation'):
-        os.mkdir('./Revocation')
-    if re.search(r"\<replaceing\>\<\!\[CDATA\[.*撤回了一条信息\]\]\>\<\/replacing\>",msg['Content'])!=None:
-        old_msg_id=re.search("\<msgid\>(.*?)\<\/msgid\>".msg['Content']).group(1)
-        old_msg=msg_dict.get(old_msg_id)
-        msg_send="您的的好友{}在{}撤回了一条信息{}，消息如下{}".format(old_msg.get('msg_from',None),old_msg.get('msg_touser',None)
-                                                     ,old_msg['type'],old_msg.get('msg_content',None))
-        if old_msg['type']=='Shatring':
-            msg_send="链接：{}".format(old_msg.get('msg_url',None))
-        elif old_msg['type']=='Picture' or 'Video' or 'Attachment' or 'Revording':
-            msg_send+="存储在当前目录revocation中"
-            shutil.move(old_msg['msg_content'],'/Revocation')
-            itchat.send(msg_send,toUserName='fileheplper')
-        msg_dict.pop(old_msg_id)
-        ClearTimeOutMsg()
 def ClearTimeOutMsg():
     if msg_dict.__len__()>0:
         for msgid in list(msg_dict):
             if time.time()-msg_dict.get(msgid,None)['msg_time']>130:
                 item=msg_dict.pop(msgid)
-                if item['type']=='Picture' or 'Video' or 'Attachment' or 'Revording':
+                if item['type'] == "Picture" \
+                        or item['type'] == "Recording" \
+                        or item['type'] == "Video" \
+                        or item['type'] == "Attachment":
+                    print("要删除的文件：", item['msg_content'])
                     os.remove(item['msg_content'])
 
 @itchat.msg_register([TEXT,PICTURE,MAP,CARD,SHARING,RECORDING,ATTACHMENT,VIDEO,FRIENDS])
@@ -57,6 +44,29 @@ def Revocation(msg):
                  "msg_url":msg_url}}
     )
     ClearTimeOutMsg()
+
+@itchat.msg_register([NOTE])
+def SaveMsg(msg):
+    if not os.path.exists('./Revocation'):
+        os.mkdir('./Revocation')
+        if re.search(r"\<replacemsg\>\<\!\[CDATA\[.*撤回了一条消息\]\]\>\<\/replacemsg\>", msg['Content']) != None:
+            old_msg_id = re.search("\<msgid\>(.*?)\<\/msgid\>", msg['Content']).group(1)
+            old_msg=msg_dict.get(old_msg_id)
+            # msg_send="您的的好友{}在{}撤回了一条信息{}，消息如下{}".format(old_msg.get('msg_from',None),old_msg.get('msg_touser',None)
+            #                                          ,old_msg['type'],old_msg.get('msg_content',None))
+            msg_send = r"您的好友：" \
+                       + old_msg.get('msg_from', None) \
+                       + r" 在 [" + old_msg.get('msg_time_touser',None) \
+                       + r"], 撤回了一条 [" + old_msg[ 'msg_type'] + "] 消息, 内容如下:" \
+                       + old_msg.get('msg_content', None)
+            if old_msg['type']=='Shatring':
+                msg_send="链接"+old_msg.get('msg_url',None)
+            elif old_msg['type']=='Picture' or old_msg['type']=='Video' or old_msg['type']=='Attachment' or old_msg['type']=='Revording':
+                msg_send+="存储在当前目录revocation中"
+                shutil.move(old_msg['msg_content'],'/Revocation')
+                itchat.send(msg_send,toUserName='filehelper')
+            msg_dict.pop(old_msg_id)
+            ClearTimeOutMsg()
 
 if __name__=='__main__':
     itchat.auto_login(hotReload=True)
